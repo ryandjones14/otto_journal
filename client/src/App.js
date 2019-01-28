881eeb79130e1ebe2b8cd3088791a672749d0550
Ryan Jones <ryandjones14@gmail.com>
2019-01-27 23:29:58 -0600

    created first components

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,110 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+const initialTasks = [
+  {
+    id: 0,
+    task: 'clean dishes',
+  },
+  {
+    id: 1,
+    task: 'wash car',
+  },
+  {
+    id: 2,
+    task: 'take out trash',
+  },
+];
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: initialTasks,
+    };
+
+    // This binding is necessary to make `this` work in the callback
+    // this.handleChange = this.handleChange.bind(this);
+    // this.addTodo = this.addTodo.bind(this);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <AddTodo placeholder='ryan'/>
+        <TaskList tasks={initialTasks} />
       </div>
     );
+  }
+}
+
+class AddTodo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      placeholder: props.placeholder,
+      value: '',
+    };
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleChange = this.handleChange.bind(this);
+    this.addTodo = this.addTodo.bind(this);    
+  }
+
+  handleChange(e) {
+    console.log(e.target.value);
+    const newValue = e.target.value;
+    this.setState(state => ({
+      value: newValue
+    }));
+  }
+  addTodo(e) {
+    console.log('value', this.state.value);
+  }
+  render() {
+    return (
+      // <form onSubmit={this.addTodo}>
+      //   <label>
+      //     Name:
+      //     <input type="text" placeholder={this.state.placeholder} value={this.state.value} onChange={this.handleChange} />
+      //   </label>
+      //   <input type="submit" value="Submit" />
+      // </form>
+      <div>
+        <input type="text" placeholder={this.state.placeholder} value={this.state.value} onChange={this.handleChange}/>
+        <button onClick={this.addTodo}>add</button>
+      </div>
+    );
+  }
+}
+
+function TaskList(props) {
+  const items = props.tasks;
+  console.log('items 1', items);
+  const listItems = items.map((item) => {
+    console.log('items 2', item.task);    
+    return (
+      <li key={item.id}>
+        <Todo task={item.task} />
+      </li>
+    );
+  });
+
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+
+class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { task: props.task };
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="checkbox"/>{this.state.task}
+      </div>
+    )
   }
 }
 
